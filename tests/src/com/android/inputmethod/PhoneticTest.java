68b03b43bd1a66db58223708b808c059f542d9c1
OfficePc-1 <dev00.maruf@gmail.com>
2023-07-27 12:25:41 +0600

    Fixed missing phonetic

--- tests/src/com/android/inputmethod/PhoneticTest.java
+++ tests/src/com/android/inputmethod/PhoneticTest.java
@@ -2,23 +2,25 @@ package com.android.inputmethod;
 
 import static org.junit.Assert.assertEquals;
 
+
 import androidx.test.filters.SmallTest;
-import androidx.test.runner.AndroidJUnit4;
 
 import com.android.inputmethod.latin.PhoneticBangla;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 
 import java.text.Normalizer;
 import java.util.Map;
-
+@SmallTest
 public class PhoneticTest {
     @Test
     public void testKeyMap() {
         PhoneticBangla phoneticBangla = new PhoneticBangla();
 
-        Map<String, String> jbrMap = phoneticBangla.jbr;
+        Map<String, String> jbrMap = phoneticBangla.kar;
         for (Map.Entry<String, String> entry : jbrMap.entrySet()) {
             String input = entry.getKey();
             String expectedOutput = entry.getValue();
